ec312367a27136b3dc6689d018d86e914b84d776
sanket <wankhdes29@gmail.com>
2023-01-25 17:04:15 +0530

    student is registering in class room

--- src/main/java/com/sanket/service/StudentService.java
+++ src/main/java/com/sanket/service/StudentService.java
@@ -1,10 +1,11 @@
 package com.sanket.service;
 
+import com.sanket.exception.ClassRoomException;
 import com.sanket.exception.StudentException;
 import com.sanket.model.Student;
 
 public interface StudentService {
 	
-	public Student registerStudent(Student student) throws StudentException;
+	public Student registerStudent(Student student, Integer classRoom) throws StudentException, ClassRoomException;
 
 }
--- src/main/java/com/sanket/service/StudentServiceImpl.java
+++ src/main/java/com/sanket/service/StudentServiceImpl.java
@@ -1,10 +1,15 @@
 package com.sanket.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sanket.exception.ClassRoomException;
 import com.sanket.exception.StudentException;
+import com.sanket.model.ClassRoom;
 import com.sanket.model.Student;
+import com.sanket.repository.ClassRoomDao;
 import com.sanket.repository.StudentDao;
 
 @Service
@@ -12,18 +17,44 @@ public class StudentServiceImpl implements StudentService {
 	
 	@Autowired
 	StudentDao studentDao;
+	
+	@Autowired
+	ClassRoomDao classRoomDao;
 
 	@Override
-	public Student registerStudent(Student student) throws StudentException {
+	public Student registerStudent(Student student, Integer classRoom) throws StudentException, ClassRoomException {
 		
 		if(student != null) {
 			
-			return studentDao.save(student);
+			List<ClassRoom> listOfClassRooms = classRoomDao.findAll();
+			
+			for(ClassRoom eachClassRooms: listOfClassRooms) {
+				
+				if(eachClassRooms.getClassRoomStandard() == classRoom) {
+					
+					student.setClassRoom(eachClassRooms); 
+					
+					eachClassRooms.getListOfStudents().add(student);
+					
+					classRoomDao.save(eachClassRooms);
+					
+					return student;
+					
+				
+			}	
 		}
-		
+			throw new ClassRoomException("Class room not found with this number " + classRoom);
+	}else {
 		
 		throw new StudentException("Please enter valid student details");
 		
 	}
+			
+			
+	
 
+		
+		
+	}
+	
 }
